f21da08b0bff19ceedfe01fb87034f98af4dd969
semanuelly <102314726+semanuelly@users.noreply.github.com>
2022-03-24 20:46:14 -0300

    Add files via upload

--- codigo.py
+++ codigo.py
@@ -1,21 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Created on Thu Mar 24 19:07:14 2022
 
-This is a temporary script file.
+@author: emanu
 """
 
-def hoffman(lista):
-    lista2=[]
-    dic={}
-    for i in lista:
-         lista2.append(lista.count(i))
-    dic = {}
-    for i,x in zip(lista, lista2):
-        dic[x] = i
-        print(dic) 
+# def hoffman(lista):
+lista=[1,3,3,3,6,4,2,2,2,2,2,2,2,8,8,8,8]
+lista2=[]
+for elemento in lista:
+    lista2.append(lista.count(elemento))
+dic = {}
+for i,y in zip(lista, lista2):
+    dic[i] = y
+    
+lista3 = list(zip(dic.keys(),dic.values()))
+lista3.sort(key=lambda x: x[1])
+dic1 = dict((z, w) for (z, w) in lista3)
+print(dic1,'\n')
 
-hoffman([1,3,3,3,6,4,2,2,2,2,2,2,2,8,8,8,8,4,4,4])
+maior = dic[max(dic1, key=dic1.get)]
 
-
+for n, a in dic.items():
+    if a == maior: 
+        principal=n
+        print(principal)
         
+
+
+
